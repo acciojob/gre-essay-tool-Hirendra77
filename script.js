0d7d40c8eddcace8f2e41417a1132dc19bba15e2
Hirendra77 <119104906+Hirendra77@users.noreply.github.com>
2023-01-30 11:47:20 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,27 +1,8 @@
 //your code here
-function count_word(val) {
-  var wom = val.split(" ") 
-	  if(wom[0]=="") return { words : 0}
-		return {
-    words: wom ? wom.length : 0,
-  };
-}
-var textContent = document.getElementById("evaluatedText");
-var showWordCount = document.getElementById("wordCount");
-
-textContent.addEventListener(
-  "input",
-  function () {
-    var v = count_word(this.value);
-    showWordCount.innerHTML = v.words;
-  },
-  false
-);
-
-
 // function count_word(val) {
-//   var wom = val.match(/\S+/g);
-//   return {
+//   var wom = val.split(" ") 
+// 	  if(wom[0]=="") return { words : 0}
+// 		return {
 //     words: wom ? wom.length : 0,
 //   };
 // }
@@ -36,3 +17,22 @@ textContent.addEventListener(
 //   },
 //   false
 // );
+
+
+function count_word(val) {
+  var wom = val.match(/\S+/g);
+  return {
+    words: wom ? wom.length : 0,
+  };
+}
+var textContent = document.getElementById("evaluatedText");
+var showWordCount = document.getElementById("wordCount");
+
+textContent.addEventListener(
+  "input",
+  function () {
+    var v = count_word(this.value);
+    showWordCount.innerHTML = v.words;
+  },
+  false
+);
