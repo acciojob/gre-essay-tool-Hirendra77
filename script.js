87488e1733037e3691913886f1660115ee1ea969
Hirendra77 <119104906+Hirendra77@users.noreply.github.com>
2023-01-30 11:43:17 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 //your code here
 function count_word(val) {
-  var wom = val.split(“ “) 
-	  if(wom[0]==“”) return { words : 0}
+  var wom = val.split(" ") 
+	  if(wom[0]=="") return { words : 0}
 		return {
     words: wom ? wom.length : 0,
   };
